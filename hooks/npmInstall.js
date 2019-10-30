2388c696a1a6e6f82b8f1ed1f27a4da52ea8d6a2
Fabian Kutschera <fabian.kutschera@gmail.com>
2019-10-30 12:48:21 +0100

    Update npmInstall.js

--- hooks/npmInstall.js
+++ hooks/npmInstall.js
@@ -22,7 +22,7 @@ const PLUGIN_ID = "cordova-plugin-openwith";
 
 module.exports = function (context) {
     var child_process = require('child_process');
-        var deferral = context.requireCordovaModule('q').defer();
+        var deferral = require('q').defer();
 
 
     console.log('Installing "' + PLUGIN_ID + '" dependencies');
